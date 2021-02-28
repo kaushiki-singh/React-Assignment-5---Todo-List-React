95e3a759f0868655d0b495106ffd30e5d81dc220
kaushiki-singh <kaushiki12.qms@gmail.com>
2021-02-28 13:39:04 +0530

    Todo Completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,66 @@ import "./../styles/App.css";
 
 function App() 
 {
+	const [item, setItem] = useState("");
+  const [displayItem, setDisplayItem] = useState([]);
+
+  const inputEvent = (event) => {
+    setItem(event.target.value);
+  }
+  const addToList = () => {
+    setDisplayItem((oldItems) => {
+      return [...oldItems, item];
+    });
+    setItem("");
+  }
+
+  const deleteItem = (id) => {
+  
+        // console.log("deleted");
+        setDisplayItem((oldItems) => {
+          return oldItems.filter((arrEle, index)=>{
+            return index !== id;
+
+          });
+        });
+  }
+  const editItem = (id, editValue) => {
+ if(editValue !== ""){
+   setDisplayItem((oldItems)=> {
+     oldItems[id] = editValue;
+     return [...oldItems];
+   });
+   
+ }
+  
+  }
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+	<div className = "main_div">
+      <div className = "center-div">
+        <br />
+        <h1>ToDo List</h1>
+        <br/>
+        <textarea id = "task" type="text" placeholder="add items" onChange={inputEvent} value={item}/>
+        <button id="btn" onClick={addToList}>+</button>
+
+        <ol>
+          {/* <li>{displayItem}</li> */}
+
+          {displayItem.map( (itemVal, index) => {
+            return <ToDoList
+              key={index}
+               id={index}
+                itemVal={itemVal}
+                onSelectDelete = {deleteItem}
+                onSelectEdit = {editItem}
+                />
+            // return <li>{itemVal}</li>
+          } )}
+        </ol>
+      </div>
+
+    </div>
 	</div>
 	);
 }
